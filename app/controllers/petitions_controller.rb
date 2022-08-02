85afa895c9ff6e85c7cdccdbd9876aaef3ea84ed
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-08-02 15:26:52 -0300

    Show petition

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -2,4 +2,8 @@ class PetitionsController < ApplicationController
   def index
     @petitions = Petition.all
   end
+
+  def show
+    @petition = Petition.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "petitions#index"
 
   get "/petitions", to: "petitions#index"
+  get "/petitions/:id", to: "petitions#show"
 end
