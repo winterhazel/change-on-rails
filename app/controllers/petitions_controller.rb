a5a67178d103b82d04757a5e33132661aba8c195
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-08-03 20:10:56 -0300

    Handle RecordNotFound for petitions

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -1,4 +1,6 @@
 class PetitionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def index
     @petitions = Petition.all
   end
@@ -66,14 +68,19 @@ class PetitionsController < ApplicationController
     @petition.destroy
     redirect_to root_path, status: :see_other
   end
+
+  private
+
+  def petition_params
+    params.require(:petition).permit(:title, :description).with_defaults(goal: 100)
+  end
+
+  def can_edit? (petition)
+    user_signed_in? && petition.user.id == current_user.id
+  end
+
+  def record_not_found
+    redirect_to petitions_path
+  end
 end
 
-private
-
-def can_edit? (petition)
-  user_signed_in? && petition.user.id == current_user.id
-end
-
-def petition_params
-  params.require(:petition).permit(:title, :description).with_defaults(goal: 100)
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {registrations: 'users/registrations'}
 
   root "application#index"
-
   get "/petitions/:id/comments", to: "comments#index"
 
   resources :petitions do
