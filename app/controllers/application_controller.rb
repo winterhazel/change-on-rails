b2073253aad99c34c82bc7b9bd19581b2ed6eb59
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-08-04 20:48:10 -0300

    CSRF countermeasure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
 
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
