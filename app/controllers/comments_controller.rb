bfeca3ed81add09d7f024eea96d23be8fd318213
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-08-04 13:40:48 -0300

    Order comments by date

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   def index
     @petition = Petition.find(params[:id])
+    @signatures = @petition.signatures.order("created_at DESC")
   end
 end
