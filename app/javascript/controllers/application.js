2982f8c82f2788d20fc85cf2318290dde1262e72
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-08-05 14:08:44 -0300

    Move hide dropdown menu script to header

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,13 +7,3 @@ application.debug = false
 window.Stimulus = application
 
 export {application}
-
-// Hide dropdown menu
-document.body.addEventListener('click', hide_dropdown_menu, true)
-
-function hide_dropdown_menu() {
-    let dropdown_menu = document.getElementById('dropdown_menu');
-    if (dropdown_menu.style.display === 'block') {
-        dropdown_menu.style.display = 'none';
-    }
-}
