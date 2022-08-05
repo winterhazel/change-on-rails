d0c025177d6c783df73ef073587c59e50a0fd319
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-08-05 12:26:10 -0300

    Add dropdown menu

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,19 @@
-import { Application } from "@hotwired/stimulus"
+import {Application} from "@hotwired/stimulus"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
-export { application }
+export {application}
+
+// Hide dropdown menu
+document.body.addEventListener('click', hide_dropdown_menu, true)
+
+function hide_dropdown_menu() {
+    let dropdown_menu = document.getElementById('dropdown_menu');
+    if (dropdown_menu.style.display === 'block') {
+        dropdown_menu.style.display = 'none';
+    }
+}
